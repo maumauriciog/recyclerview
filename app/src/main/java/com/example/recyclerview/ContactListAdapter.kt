4e3b0da2d7b857833de241b2991ed1cf871006e5
Mauricio Gomes das Chagas <maumauriciog@hotmail.com>
2024-07-19 16:05:57 -0300

    Change adapter 2.4 (change class ActivityMain.kt)

--- app/src/main/java/com/example/recyclerview/ContactListAdapter.kt
+++ app/src/main/java/com/example/recyclerview/ContactListAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.recyclerview
 
+import android.content.DialogInterface.OnClickListener
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,34 +11,45 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 
 
-class ContactListAdapter : ListAdapter<dtContact, ContactListAdapter.ContactViewHolder>(ContactDiffUtils()){
+class ContactListAdapter :
+    ListAdapter<dtContact, ContactListAdapter.ContactViewHolder>(ContactDiffUtils()) {
+
+    // variável do tipo função. Utiliza-se a seguinte sintaxe: variável: () -> Unit
+    // esta variável recebe como parâmetro o data class
+    private lateinit var onClickListener: (dtContact) -> Unit
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
-    val view = LayoutInflater.from(parent.context).inflate(R.layout.itens_recyclerview, parent,false)
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.itens_recyclerview, parent, false)
         return ContactViewHolder(view)
     }
-
     override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
-        holder.bind(getItem(position))
+        holder.bind(getItem(position), onClickListener)
     }
 
-    class ContactViewHolder(view: View) : RecyclerView.ViewHolder(view){
+    fun setOnClickListener(onClick: (dtContact) -> Unit) {
+        onClickListener = onClick
+    }
+
+    class ContactViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         private val tvName = view.findViewById<TextView>(R.id.tv_name)
         private val tvPhone = view.findViewById<TextView>(R.id.tv_phone)
         private val image = view.findViewById<ImageView>(R.id.img)
 
-        fun bind(contact: dtContact){
+        fun bind(contact: dtContact) {
             tvName.text = contact.name
             tvPhone.text = contact.phone
             image.setImageResource(contact.icon)
         }
     }
 
-    class ContactDiffUtils : DiffUtil.ItemCallback<dtContact>(){
+    class ContactDiffUtils : DiffUtil.ItemCallback<dtContact>() {
         override fun areItemsTheSame(oldItem: dtContact, newItem: dtContact): Boolean {
             return newItem == oldItem
         }
+
         override fun areContentsTheSame(oldItem: dtContact, newItem: dtContact): Boolean {
-         return newItem.name == oldItem.name
+            return newItem.name == oldItem.name
         }
     }
 }
